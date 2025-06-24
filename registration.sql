7674cedec292087ee701346c8ebf4f2e3c6b7005
mariyadivya03 <mariyadivya2612@gmail.com>
2025-06-24 19:34:11 +0530

    Add files via upload

--- registration.sql
+++ registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2025 at 10:26 AM
+-- Generation Time: Jun 24, 2025 at 04:01 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,19 +34,20 @@ CREATE TABLE `registration` (
   `Gender` enum('Male','Female','Others') NOT NULL,
   `Email` varchar(50) NOT NULL,
   `Password` varchar(20) NOT NULL,
-  `PhoneNo` bigint(10) NOT NULL
+  `PhoneNo` bigint(10) NOT NULL,
+  `Course` varchar(50) NOT NULL,
+  `Languages` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `registration`
 --
 
-INSERT INTO `registration` (`id`, `FirstName`, `LastName`, `Gender`, `Email`, `Password`, `PhoneNo`) VALUES
-(1, 'Mariya', 'Divya', 'Female', 'divya@gmail.com', 'divya', 6381047529),
-(2, 'Raja', 'Jenifer', 'Male', 'jeri@gmail.com', 'jeri', 7418000108),
-(3, 'Rani', 'Pushpum', 'Female', 'rani@gamil.com', 'rani', 9789368501),
-(4, 'Stephan', 'Thanaraj', 'Male', 'stephen@gmail.com', 'stephan', 9789368501),
-(5, 'Jegatheesh', 's', 'Male', 'jega@gmail.com', 'jega', 6369948366);
+INSERT INTO `registration` (`id`, `FirstName`, `LastName`, `Gender`, `Email`, `Password`, `PhoneNo`, `Course`, `Languages`) VALUES
+(1, 'Mariya', 'Divya', 'Female', 'divya@2003', 'div', 6381047529, 'MCA', 'Tamil, English'),
+(2, 'Aashika', 'Banu', 'Female', 'ash@gmail.com', 'ash', 9789368501, 'MCA', 'Tamil, English'),
+(3, 'Mohamed', 'Shakapdeen', 'Male', 'sha@gmail.com', 'sha', 6374180001, 'BCA', 'Tamil, English'),
+(4, 'Maha', 'Divya', 'Female', 'maha@gmail.com', 'maha', 9834120923, 'B.Tech IT', 'Tamil, English');
 
 --
 -- Indexes for dumped tables
@@ -66,7 +67,7 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
