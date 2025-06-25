8c7d0149123273e8eec0baea93672b737bdd0a3d
mariyadivya03 <mariyadivya2612@gmail.com>
2025-06-25 14:42:58 +0530

    Add files via upload

--- registration.sql
+++ registration.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2025 at 04:01 PM
+-- Generation Time: Jun 25, 2025 at 11:11 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -33,21 +33,21 @@ CREATE TABLE `registration` (
   `LastName` varchar(50) NOT NULL,
   `Gender` enum('Male','Female','Others') NOT NULL,
   `Email` varchar(50) NOT NULL,
-  `Password` varchar(20) NOT NULL,
+  `Password` varchar(250) DEFAULT NULL,
   `PhoneNo` bigint(10) NOT NULL,
   `Course` varchar(50) NOT NULL,
-  `Languages` varchar(250) NOT NULL
+  `Languages` varchar(250) NOT NULL,
+  `Resume` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `registration`
 --
 
-INSERT INTO `registration` (`id`, `FirstName`, `LastName`, `Gender`, `Email`, `Password`, `PhoneNo`, `Course`, `Languages`) VALUES
-(1, 'Mariya', 'Divya', 'Female', 'divya@2003', 'div', 6381047529, 'MCA', 'Tamil, English'),
-(2, 'Aashika', 'Banu', 'Female', 'ash@gmail.com', 'ash', 9789368501, 'MCA', 'Tamil, English'),
-(3, 'Mohamed', 'Shakapdeen', 'Male', 'sha@gmail.com', 'sha', 6374180001, 'BCA', 'Tamil, English'),
-(4, 'Maha', 'Divya', 'Female', 'maha@gmail.com', 'maha', 9834120923, 'B.Tech IT', 'Tamil, English');
+INSERT INTO `registration` (`id`, `FirstName`, `LastName`, `Gender`, `Email`, `Password`, `PhoneNo`, `Course`, `Languages`, `Resume`) VALUES
+(1, 'Mariya', 'Divya', 'Female', 'div@gmail.com', '$2y$10$FaTPSFT567KlU1rPnhv0uOtEyZnXthdK2Q/VzTKAr6bIgAOcmn4Gm', 6381047529, 'MCA', 'Tamil, English', '1750839299_S.Mariya Divya(Resume).pdf'),
+(2, 'Raja', 'Jenifer', 'Male', 'jeri@gmail.com', '$2y$10$U/XutO2R9UtuXRAC2lD4KemWY/pmvIrUzstQ7p/5RNvdtEjaFIq16', 7418000108, 'B.Sc CS', 'Tamil, English', '1750841727_S.Mariya Divya(Resume).docx'),
+(3, 'Aashika', 'Banu', 'Female', 'ash@gmail.com', '$2y$10$N/LB/Az0SaFVUt8NQZ2hyOBYZ7Pr/9OIOJywO/CuoFmnGTEJwcAl6', 9812046209, 'MCA', 'Tamil, English', '1750842251_Divya(Resume).pdf');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +67,7 @@ ALTER TABLE `registration`
 -- AUTO_INCREMENT for table `registration`
 --
 ALTER TABLE `registration`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
